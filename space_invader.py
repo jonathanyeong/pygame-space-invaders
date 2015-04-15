63952db719cae0e0ebb15abced63eea369bd225d
Jonathan Yeong <j.yeong@neura.edu.au>
2015-04-15 16:23:52 +1000

    Get a basic window to show with some text
    This is basic code copied from:
    http://www.pygame.org/docs/tut/tom/games2.html#AEN51. It will currently
    show a black window with "space invaders" text.

---
+++ space_invader.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import os, sys
+import pygame
+from pygame.locals import *
+
+def main():
+    # Initialise screen
+    pygame.init()
+    screen = pygame.display.set_mode((600, 480))
+    pygame.display.set_caption("PyGame Space Invaders clone")
+
+    # Fill background
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((10, 10, 10))
+
+    # Display some text
+    font = pygame.font.Font(None, 36)
+    text = font.render("Space Invaders", 1, (250, 250, 250))
+    textpos = text.get_rect()
+    textpos.centerx = background.get_rect().centerx
+    background.blit(text, textpos)
+
+    # Blit everything to the screen
+    screen.blit(background, (0, 0))
+    pygame.display.flip()
+
+    # Event loop
+    while 1:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                return
+
+        screen.blit(background, (0, 0))
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    main()
