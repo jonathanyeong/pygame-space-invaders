0c5759b3e268e117fd3c8fd5190dc89bfc51cc3e
Jonathan Yeong <j.yeong@neura.edu.au>
2015-04-17 10:23:32 +1000

    Begin refactoring the program
    Refactoring now while it's small.
    
    Changes made:
    - Move main method into a class named "space invaders". This will be
      more useful later on.
    - Move the initialising game methods into an init method.

--- space_invader.py
+++ space_invader.py
@@ -4,41 +4,44 @@ import os, sys
 import pygame
 from pygame.locals import *
 
-def main():
-    # Initialise screen
-    pygame.init()
-    screen = pygame.display.set_mode((600, 480))
-    pygame.display.set_caption("PyGame Space Invaders clone")
+class SpaceInvader(object):
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((600, 480))
+        pygame.display.set_caption("PyGame Space Invaders clone")
 
-    # Fill background
-    background = pygame.Surface(screen.get_size())
-    background = background.convert()
-    background.fill((10, 10, 10))
+        # Fill background
+        self.background = pygame.Surface(self.screen.get_size())
+        self.background = self.background.convert()
+        self.background.fill((10, 10, 10))
 
-    font = pygame.font.Font(None, 42) # Init some font object
-    # Display title text
-    title_text = font.render("Space Invaders", 1, (250, 250, 250))
-    textpos = title_text.get_rect()
-    textpos.centerx = background.get_rect().centerx
-    background.blit(title_text, textpos)
-    # Display instruction text
-    instruction_text = font.render("Press B to start", 1, (250, 250, 250))
-    instruction_textpos = instruction_text.get_rect()
-    instruction_textpos.midbottom = background.get_rect().midbottom
-    background.blit(instruction_text, instruction_textpos)
+        self.font = pygame.font.Font(None, 42) # Init some font object
 
-    # Blit everything to the screen
-    screen.blit(background, (0, 0))
-    pygame.display.flip()
+    def main(self):
+        # Initialise screen
+        # Display title text
+        title_text = self.font.render("Space Invaders", 1, (250, 250, 250))
+        textpos = title_text.get_rect()
+        textpos.centerx = self.background.get_rect().centerx
+        self.background.blit(title_text, textpos)
+        # Display instruction text
+        instruction_text = self.font.render("Press B to start", 1, (250, 250, 250))
+        instruction_textpos = instruction_text.get_rect()
+        instruction_textpos.midbottom = self.background.get_rect().midbottom
+        self.background.blit(instruction_text, instruction_textpos)
 
-    # Event loop
-    while 1:
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                return
-
-        screen.blit(background, (0, 0))
+        # Blit everything to the screen
+        self.screen.blit(self.background, (0, 0))
         pygame.display.flip()
 
+        # Event loop
+        while 1:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    return
+
+            self.screen.blit(self.background, (0, 0))
+            pygame.display.flip()
+
 if __name__ == '__main__':
-    main()
+    game = SpaceInvader().main()
