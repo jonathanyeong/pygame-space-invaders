d932cda7df29bcd993fd04e1a8272710652410c9
Jonathan Yeong <j.yeong@neura.edu.au>
2015-04-17 13:05:29 +1000

    Format styling to fit pep8 guidelines
    This fixes all the styling issues except for some long lines which is
    unavoidable.

--- space_invader.py
+++ space_invader.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 
-import os, sys
+import os
+import sys
 import pygame
 from pygame.locals import *
+
 
 class SpaceInvader(object):
     def __init__(self):
         pygame.init()
+        # Initialise screen
         self.screen = pygame.display.set_mode((600, 480))
         pygame.display.set_caption("PyGame Space Invaders clone")
 
@@ -15,16 +18,18 @@ class SpaceInvader(object):
         self.background = self.background.convert()
         self.background.fill((10, 10, 10))
 
-        self.font = pygame.font.Font(None, 42) # Init some font object
+        self.font = pygame.font.Font(None, 42)  # Init some font object
 
     def draw_text(self, render_text, pos):
         text = self.font.render(render_text, 1, (250, 250, 250))
         textpos = text.get_rect()
         (x, y) = pos
 
-        if (y == 0): # We know that this is at the top of the screen
+        if (y == 0):
+            # We know that this is at the top of the screen
             textpos.midtop = pos
-        elif (y == self.background.get_rect().height): # We know that this is the bottom of screen
+        elif (y == self.background.get_rect().height):
+            # We know that this is the bottom of screen
             textpos.midbottom = pos
         else:
             textpos.center = pos
@@ -33,10 +38,12 @@ class SpaceInvader(object):
 
     def main_menu_text(self):
         # Display title text
-        (title_text, textpos) = self.draw_text("Space Invaders", self.background.get_rect().midtop)
+        (title_text, textpos) = self.draw_text("Space Invaders",
+                                               self.background.get_rect().midtop)
         self.background.blit(title_text, textpos)
         # Display instruction text
-        (instruction_text, textpos) = self.draw_text("Press B to start",self.background.get_rect().midbottom);
+        (instruction_text, textpos) = self.draw_text("Press B to start",
+                                                     self.background.get_rect().midbottom)
         self.background.blit(instruction_text, textpos)
 
     def run(self):
