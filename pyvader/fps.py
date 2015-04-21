b7e09d8a108fcbbf11d595f7cbf73d1a73894670
Jonathan Yeong <j.yeong@neura.edu.au>
2015-04-21 13:52:43 +1000

    Fix styling with fps class according to pep8

--- pyvader/fps.py
+++ pyvader/fps.py
@@ -1,9 +1,10 @@
 import pygame
 
+
 class Fps(object):
     def __init__(self):
         self.fps = 60
         self.clock = pygame.time.Clock()
-    
+
     def tick(self):
         return self.clock.tick(self.fps)
