8ab15ea53059abc25a023b67e98ee4ff9e403f99
Jonathan Yeong <j.yeong@neura.edu.au>
2015-04-21 13:48:07 +1000

    Begin refactoring player class
    Make the init method smaller.

--- pyvader/player.py
+++ pyvader/player.py
@@ -4,73 +4,77 @@ import fps
 
 class Player(object):
     def __init__(self, (screen_x, screen_y)):
-        self._sprite = pygame.image.load("pyvader/assets/images/player_ship.png")
-        self._missile_sprite = pygame.image.load("pyvader/assets/images/missile.png")
-        # Probably need to resize based on screen dimensions
-        # Have a break point for scaling
-        # if screen_x > some number: Do this scaling
-        # Otherwise: Do this scaling
-        self._sprite = pygame.transform.scale(self._sprite, (26, 16))
-        self._lives = 3
-        # Player starts in the middle of the screen
-        self._currX = screen_x/2
-        margin = 10  # Arbitrary number
-        self._currY = screen_y - self._sprite.get_rect().height - margin
-        self._speed = 15  # Some arbitrary speed of movement
-        self._boundary = screen_x 
-        # Firing information
-        self._missile_sprite = pygame.transform.scale(self._missile_sprite,
+        self.player_sprite = pygame.image.load("pyvader/assets/images/player_ship.png")
+        self.missile_sprite = pygame.image.load("pyvader/assets/images/missile.png")
+        # Sprite manipulation
+        self.player_sprite = pygame.transform.scale(self.player_sprite, (26, 16))
+        self.missile_sprite = pygame.transform.scale(self.missile_sprite,
                                                       (4, 10))
-        self._missile_xpos = self._currX + (self._sprite.get_width() / 2)
-        self._missile_ypos = self._currY
-        self._missile_speed = 6
-        self._is_firing = False
+        # Other properties
+        self.screen_width = screen_x
+        self.screen_height = screen_y
+        # Player and missile properties
+        self.__initial_player_properties()
+        self.__initial_missile_properties()
+
+    def __initial_player_properties(self):
+        self.currX = self.screen_width/2
+        margin = 10  # Arbitrary number
+        self.currY = self.screen_height - self.player_sprite.get_rect().height - margin
+        self.lives = 3
+        self.speed = 15  # Some arbitrary speed of movement
+
+    def __initial_missile_properties(self):
+        self.missile_xpos = self.currX + (self.player_sprite.get_width() / 2)
+        self.missile_ypos = self.currY
+        self.missile_speed = 6
+        self.is_firing = False
 
     def get_lives(self):
-        return self._lives
+        return self.lives
 
     def fire_thread(self):
         clock = fps.Fps()
-        while (self._is_firing == True) and (-1*(self._missile_ypos) < 0):
-            self._missile_ypos -= self._missile_speed
+        while (self.is_firing == True) and (-1*(self.missile_ypos) < 0):
+            self.missile_ypos -= self.missile_speed
             clock.tick()
 
-        self._is_firing = False
-        self._missile_xpos = self._currX + (self._sprite.get_width() / 2)
-        self._missile_ypos = self._currY
+        self.is_firing = False
+        self.missile_xpos = self.currX + (self.player_sprite.get_width() / 2)
+        self.missile_ypos = self.currY
 
     def fire(self):
-        if self._is_firing != True:
+        if self.is_firing != True:
             # Update missile x, y before starting thread
-            self._missile_xpos = self._currX + (self._sprite.get_width() / 2)
-            self._missile_ypos = self._currY
-            self._is_firing = True
+            self.missile_xpos = self.currX + (self.player_sprite.get_width() / 2)
+            self.missile_ypos = self.currY
+            self.is_firing = True
             thread.start_new_thread(self.fire_thread, ())
 
     def take_damage(self):
-        self._lives -= 1
+        self.lives -= 1
 
     def position(self):
-        return (self._currX, self._currY)
+        return (self.currX, self.currY)
 
     def move_right(self):
-        sprite_width = self._sprite.get_rect().width
-        if (self._currX < (self._boundary - sprite_width*1.5)):
-            self._currX += self._speed
+        sprite_width = self.player_sprite.get_rect().width
+        if (self.currX < (self.screen_width - sprite_width*1.5)):
+            self.currX += self.speed
         return self.position()
 
     def move_left(self):
-        if (self._currX > 0):
-            self._currX -= self._speed
+        if (self.currX > 0):
+            self.currX -= self.speed
         return self.position()
 
     def render(self, background):
-        background.blit(self._sprite, self.position())
-        if self._is_firing == True:
-            background.blit(self._missile_sprite, (self._missile_xpos,
-                                              self._missile_ypos))
+        background.blit(self.player_sprite, self.position())
+        if self.is_firing == True:
+            background.blit(self.missile_sprite, (self.missile_xpos,
+                                              self.missile_ypos))
         return background 
 
     # For testing purposes
     def get_speed(self):
-        return self._speed
+        return self.speed
