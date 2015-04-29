7142cfff88de08fd644dd86461fe66f060b56a56
Jonathan Yeong <j.yeong@neura.edu.au>
2015-04-29 16:07:10 +1000

    Fix window resizing issue
    When you went fullscreen the size didn't actually change. It was like
    you just stretched the window rather than resize it. This is fixed in
    this commit. Also the commit gets a scaling factor that will be used to
    scale the images.

--- pyvader/space_invader.py
+++ pyvader/space_invader.py
@@ -8,11 +8,13 @@ from IPython import embed
 from pygame.locals import *
 
 
+INITIAL_WIDTH=1080
+INITIAL_HEIGHT=720
 class SpaceInvader(object):
     def __init__(self):
         pygame.init()
         # Initialise screen
-        self.screen = pygame.display.set_mode((0,0), pygame.DOUBLEBUF | pygame.FULLSCREEN)
+        self.screen = pygame.display.set_mode((INITIAL_WIDTH, INITIAL_HEIGHT), DOUBLEBUF | RESIZABLE)
         pygame.display.set_caption("PyGame Space Invaders clone")
         pygame.mouse.set_visible(0)
         pygame.key.set_repeat(10, 10)
@@ -28,6 +30,12 @@ class SpaceInvader(object):
         self.player = player.Player((max_width, max_height))
         self.barricade = barricade.Barricade()
         self.font = pygame.font.Font(None, 42)  # Init some font object
+
+    def get_scaling(self):
+        (screen_width, screen_height) = self.screen.get_size()
+        scale_x = float(screen_width)/INITIAL_WIDTH
+        scale_y = float(screen_height)/INITIAL_HEIGHT
+        return (scale_x, scale_y)
 
     def draw_text(self, render_text, pos):
         text = self.font.render(render_text, 1, (250, 250, 250))
@@ -56,9 +64,9 @@ class SpaceInvader(object):
         self.background.blit(instruction_text, textpos)
 
     def resize_screen(self):
-        self.screen = pygame.display.set_mode((800,680),pygame.DOUBLEBUF) 
+        [size] = pygame.display.list_modes()[:1]
+        self.screen = pygame.display.set_mode(size, DOUBLEBUF | FULLSCREEN) 
         self.background = pygame.Surface(self.screen.get_size())
-        self.background = self.background.convert()
         self.background.fill((10, 10, 10))
         (max_width, max_height) = self.screen.get_size()
         # We need to take into account the size of the player sprite.
@@ -67,9 +75,9 @@ class SpaceInvader(object):
     def run(self):
         # Worry about FPS tick later
         START_GAME = False  # ie Main Menu
+        bg_center = self.background.get_rect().center
         self.screen.blit(self.background, (0, 0))
         pygame.display.flip()
-        bg_center = self.background.get_rect().center
         # Event loop
         while 1:
             if START_GAME is False:
@@ -78,18 +86,19 @@ class SpaceInvader(object):
 
                 for event in pygame.event.get():
                     if event.type == QUIT:
-                        return
+                        self.quit_game()
                     if event.type == KEYDOWN:
                         if event.key == K_SLASH:
                             self.resize_screen()
+                            bg_center = self.background.get_rect().center
+                            print "Scaling is: ", self.get_scaling()
                         if event.key == K_ESCAPE:
-                            return
+                            self.quit_game()
                         if event.key == K_b:
                             print "start game"
                             START_GAME = True
                             break
                         # Option to load full screen as well
-
                 self.background.blit(logo, logo.get_rect(center=bg_center))
                 self.screen.blit(self.background, (0, 0))
                 pygame.display.flip()
@@ -98,10 +107,10 @@ class SpaceInvader(object):
 
                 for event in pygame.event.get():
                     if event.type == QUIT:
-                        return
+                        self.quit_game()
                     if event.type == KEYDOWN:
                         if event.key == K_ESCAPE:
-                            return
+                            self.quit_game()
                 keys = pygame.key.get_pressed()
                 if keys[K_d]:
                     self.player.move_right()
@@ -122,6 +131,11 @@ class SpaceInvader(object):
                 self.screen.blit(self.background, (0, 0))
                 self.screen.blit(player_layer, (0, 0))
                 pygame.display.flip()
+
+    def quit_game(self):
+        pygame.quit()
+        sys.exit()
+
 
 if __name__ == '__main__':
     game = SpaceInvader().run()
