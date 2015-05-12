b94b1877d5ccb14abfcfbe0dc850f18ec5e36a62
Jonathan Yeong <j.yeong@neura.edu.au>
2015-05-12 14:10:19 +1000

    Add a prototype file
    This is to be used for quick testing of concepts. It is currently
    testing the idea of updating a certain area of the screen instead of
    updating the entire screen.
    
    Unfortunately, there is an issue where it flickers.

---
+++ prototype/main.py
@@ -0,0 +1,80 @@
+import pygame
+import os
+import sys
+from pygame.locals import *
+
+class Player:
+    def __init__(self):
+        self.sprite = pygame.image.load("assets/images/player_ship.png")
+        self.sprite = pygame.transform.scale(self.sprite, (26, 16))
+        self.speed = 10
+
+    def set_position(self, width, height):
+        self.width = width
+        self.height = height
+        self.x = width/2
+        self.y = height - self.sprite.get_rect().height - 10
+
+    def get_position(self):
+        return (self.x, self.y)
+
+    def move_right(self):
+        sprite_width = self.sprite.get_rect().width
+        if (self.x < self.width - sprite_width*1.5):
+            self.x += self.speed
+        return self.get_position()
+
+    def move_left(self):
+        if (self.x > 0):
+            self.x -= self.speed
+        return self.get_position()
+
+    def get_player_layer(self):
+        return self.sprite
+
+class Pyvader:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1080, 720), DOUBLEBUF)
+        pygame.display.set_caption("Pygame Space Invader Prototype")
+        self.screen.fill((0,0,0))
+        self.player = Player()
+        self.player.set_position(self.screen.get_rect().width, 
+                                  self.screen.get_rect().height)
+        self.run()
+
+    def run(self):
+        while 1:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+
+
+            keys = pygame.key.get_pressed()
+            if keys[K_d]:
+                (x, y) = self.player.get_position()
+                pRect = self.player.get_player_layer()
+                player_rect = pygame.Rect(x, y, pRect.get_rect().width, pRect.get_rect().height)
+                pygame.draw.rect(self.screen, (0,0,0), player_rect)
+                pygame.display.update(player_rect)
+                self.player.move_right()
+            if keys[K_a]:
+                (x, y) = self.player.get_position()
+                pRect = self.player.get_player_layer()
+                player_rect = pygame.Rect(x, y, pRect.get_rect().width, pRect.get_rect().height)
+                pygame.draw.rect(self.screen, (0,0,0), player_rect)
+                pygame.display.update(player_rect)
+                self.player.move_left()
+            
+            (x, y) = self.player.get_position()
+            pRect = self.player.get_player_layer()
+            self.screen.blit(pRect, (x, y))
+            pygame.display.update()
+
+if __name__ == "__main__":
+    pyvader = Pyvader()
